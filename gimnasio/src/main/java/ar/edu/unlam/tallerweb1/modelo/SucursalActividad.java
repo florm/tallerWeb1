4e3c0270db2a6d173fb9f269ab1fea506d457243
florm <florenciamartin05@gmail.com>
2018-06-20 13:26:31 -0300

    modifico tipo de dato de dia y hora de sucursal actividad,, agrego relacion a profesor

--- gimnasio/src/main/java/ar/edu/unlam/tallerweb1/modelo/SucursalActividad.java
+++ gimnasio/src/main/java/ar/edu/unlam/tallerweb1/modelo/SucursalActividad.java
@@ -19,77 +19,98 @@ public class SucursalActividad {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idSucursalActividad;
-	private Date dia;
-	private Date horaDesde;
-	private Date horaHasta;
+	private String dia;
+	private String horaDesde;
+	private String horaHasta;
 	private Integer cupo;
-	
+
 	@ManyToOne
 	private Actividad actividad;
-	
-	@ManyToOne 
+
+	@ManyToOne
 	private Sucursal sucursal;
-	
-	//Falta Relacion a Profesor
-	
-	//ManyToMany con Socio
+
+	@ManyToOne
+	private Profesor profesor;
+
+	public Profesor getProfesor() {
+		return profesor;
+	}
+
+	public void setProfesor(Profesor profesor) {
+		this.profesor = profesor;
+	}
+
+	// ManyToMany con Socio
 	@ManyToMany(cascade = { CascadeType.ALL })
-    @JoinTable(
-        name = "SocioSucursalActividad", 
-        joinColumns = @JoinColumn(name = "idSucursalActividad") , //idActividadSucursal
-        inverseJoinColumns = @JoinColumn(name = "idSocio") //idSocio
-    )
+	@JoinTable(name = "SocioSucursalActividad", joinColumns = @JoinColumn(name = "idSucursalActividad"), // idActividadSucursal
+			inverseJoinColumns = @JoinColumn(name = "idSocio") // idSocio
+	)
 	private Set<Socio> socios = new HashSet<>();
 	//
-	
+
 	public Long getIdSucursalActividad() {
 		return idSucursalActividad;
 	}
+
 	public void setIdSucursalActividad(Long id) {
 		this.idSucursalActividad = id;
 	}
-	public Date getDia() {
+
+	public String getDia() {
 		return dia;
 	}
-	public void setDia(Date dia) {
+
+	public void setDia(String dia) {
 		this.dia = dia;
 	}
-	public Date getHoraDesde() {
+
+	public String getHoraDesde() {
 		return horaDesde;
 	}
-	public Date getHoraHasta() {
+
+	public String getHoraHasta() {
 		return horaHasta;
 	}
-	public void setHoraDesde(Date horaDesde) {
+
+	public void setHoraDesde(String horaDesde) {
 		this.horaDesde = horaDesde;
 	}
-	public void setHoraHasta(Date horaHasta) {
+
+	public void setHoraHasta(String horaHasta) {
 		this.horaHasta = horaHasta;
 	}
+
 	public Integer getCupo() {
 		return cupo;
 	}
+
 	public void setCupo(Integer cupo) {
 		this.cupo = cupo;
 	}
+
 	public Actividad getActividad() {
 		return actividad;
 	}
+
 	public void setActividad(Actividad actividad) {
 		this.actividad = actividad;
 	}
+
 	public Sucursal getSucursal() {
 		return sucursal;
 	}
+
 	public void setSucursal(Sucursal sucursal) {
 		this.sucursal = sucursal;
 	}
+
 	public Set<Socio> getSocios() {
 		return socios;
 	}
+
 	public void setSocios(Set<Socio> socios) {
 		this.socios = socios;
 	}
-	
 
 }
