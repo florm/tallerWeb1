43201322867f7bcea25ab7a64d2fb08d66d48256
florm <florenciamartin05@gmail.com>
2018-06-20 14:05:20 -0300

    agrego script para creacion de datos

--- baseDeDatos/scriptResumido.sql
+++ baseDeDatos/scriptResumido.sql
@@ -1,6 +1,5 @@
 use gimnasio;
 
-
 -- tabla provincia
 Insert Into Provincia (Nombre) Values ('Buenos Aires');
 Insert Into Provincia (Nombre) Values ('Capital Federal');
@@ -17,12 +16,15 @@ values('Maipu', '1005','Sucursal Centro','850','Argentina', null, 2);
 -- tabla usuario
 insert into usuario(email, password, rol)
 value("matisuarez@gmail.com", "123456", null);
+insert into usuario(email, password, rol)
+value("jlopez@gmail.com", "123456", null);
 
 -- tabla socio
 insert into Socio(apellido, dni, domicilioCalle, domicilioDepto, domicilioNumero, mail, nombre, pais, telefono, ciudad_id, pase_id, provincia_id, sucursal_id, usuario_id)
 values('Suarez', '29941591','oro',null,'1813', 'matisuarez@gmail.com', 'Matias', 'Argentina', '1544896790',1, null, 1, 1, 1);
 insert into Socio(apellido, dni, domicilioCalle, domicilioDepto, domicilioNumero, mail, nombre, pais, telefono, ciudad_id, pase_id, provincia_id, sucursal_id, usuario_id)
-values('Lopez', '40345678','suipacha',null,'300', 'jlopez@gmail.com', 'Julieta', 'Argentina','1593457123', null, null, 2, 2, null);
+values('Lopez', '40345678','suipacha',null,'300', 'jlopez@gmail.com', 'Julieta', 'Argentina','1593457123', null, null, 2, 2, 2);
+
 
 -- tabla pase
 insert into Pase(cantidadActividades, nombre, precio)
@@ -68,21 +70,31 @@ values ('Yoga');
 insert into actividad(nombre)
 values ('Spinning');
 
+-- tabla profesor
+insert into profesor(apellido, nombre)
+values('Maria','Lopez');
+insert into profesor(apellido, nombre)
+values('Juan','Fernandez');
+insert into profesor(apellido, nombre)
+values('Laura','Rodriguez');
+insert into profesor(apellido, nombre)
+values('Diego','Castro');
+insert into profesor(apellido, nombre)
+values('Luciano','Diaz');
+
+
 -- tabla sucursal actividad
-insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id, sucursal_id)
-values (5,null,null,null,1,2);
-insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id, sucursal_id)
-values (2,null,null,null,2,2);
+insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id,profesor_id, sucursal_id)
+values (5,'Lunes','10','11',1,1,2);
+insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id,profesor_id, sucursal_id)
+values (2,'Martes','8','9',2,2,2);
 
-insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id, sucursal_id)
-values (5,null,null,null,1,1);
-insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id, sucursal_id)
-values (2,null,null,null,2,1);
-insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id, sucursal_id)
-values (10,null,null,null,3,1);
-insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id, sucursal_id)
-values (15,null,null,null,4,1);
-
-
-
+insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id,profesor_id, sucursal_id)
+values (5,'Miercoles','20','21',1,3,1);
+insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id,profesor_id, sucursal_id)
+values (2,'Lunes','9','10',2,4,1);
+insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id,profesor_id, sucursal_id)
+values (10,'Viernes','15','16',3,5,1);
+insert into sucursalactividad (cupo, dia, horaDesde, horaHasta, actividad_id,profesor_id, sucursal_id)
+values (15,'Sabado','11','12',4,1,1);
 
