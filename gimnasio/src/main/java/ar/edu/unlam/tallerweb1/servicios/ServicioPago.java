992fefb453b9f637e0cec3aa6eb2a8960acaf38a
AlejandraCasuscelli <alejandracasuscelli@gmail.com>
2018-07-13 16:56:43 -0300

    modificacion en fecha de pago

--- gimnasio/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioPago.java
+++ gimnasio/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioPago.java
@@ -1,5 +1,6 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
@@ -14,5 +15,5 @@ public interface ServicioPago {
 	public Double getTotalRecaudado(List<Pago> pagos);
 	public List<Descuento> listarDescuentos();
 	public List<Descuento> listarDescuentosConImporte(Double importe, Socio socio);
-	public void abonarPase(Long idSocio, Long idPase, Long idDescuento);
+	public void abonarPase(Long idSocio, Long idPase, Long idDescuento) throws ParseException;
 }
