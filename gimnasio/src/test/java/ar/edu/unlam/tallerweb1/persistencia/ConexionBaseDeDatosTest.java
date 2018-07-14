484737d8ae1204b673f78d1c4cd9169fa3f9de69
florm <florenciamartin05@gmail.com>
2018-07-14 17:43:28 -0300

    agrego test mock

--- gimnasio/src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionBaseDeDatosTest.java
+++ gimnasio/src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionBaseDeDatosTest.java
@@ -21,116 +21,22 @@ import org.hibernate.criterion.Restrictions;
 // Clase que prueba la conexion a la base de datos. Hereda de SpringTest por lo que corre dentro del contexto
 // de spring
 public class ConexionBaseDeDatosTest extends SpringTest{
-/*
+
     @Test
     @Transactional @Rollback(true)
     public void pruebaConexion(){
         assertThat(getSession().isConnected()).isTrue();
     }
     
-    @SuppressWarnings({ "unused", "unchecked" })
-	@Test
-    @Transactional @Rollback(true)
-    public void testListaSocios(){
-        Session sesion = getSession();
-        
-        Socio socio1 = new Socio();
-        socio1.setNombre("martin");
-        Socio socio2 = new Socio();
-        socio2.setNombre("juan");
-        
-        Socio socio3 = new Socio();
-        socio3.setNombre("ana");
-        
-        sesion.save(socio1);
-        sesion.save(socio2);
-        sesion.save(socio3);
-        
-        Sucursal sucursal1 = new Sucursal();
-        sucursal1.setId(1L);
-        
-        Sucursal sucursal2 = new Sucursal();
-        sucursal1.setId(2L);
-        
-        sesion.save(sucursal1);
-        sesion.save(sucursal2);
-        
-        socio1.setSucursal(sucursal1);
-        socio2.setSucursal(sucursal1);
-        socio3.setSucursal(sucursal2);
-        
-        
-		List<Socio> listaDeSocios = sesion.createCriteria(Socio.class)
-				.createAlias("sucursal", "buscaSucursal")
-				.add(Restrictions.eq("buscaSucursal.id", 2L))
-				.list();
-		
-		assertEquals(1, listaDeSocios.size());
-        
-        
-        
-    }
-    @SuppressWarnings({ "unused", "unchecked" })
-	@Test
-    @Transactional @Rollback(true)
-    public void testBuscaSocio(){
-    	Session sesion = getSession();
-    	Usuario usuario1 = new Usuario();
-    	usuario1.setNick("nick1");
-    	usuario1.setPassword("pass1");
-    	sesion.save(usuario1);
-        
-        Socio socio1 = new Socio();
-        socio1.setNombre("martin");
-        socio1.setUsuario(usuario1);
-        Socio socio2 = new Socio();
-        socio2.setNombre("juan");
-        
-        sesion.save(socio1);
-        sesion.save(socio2);
-        
-        Socio socio =  (Socio) sesion.createCriteria(Socio.class)
-    			.createAlias("usuario", "buscarPorUsuario")
-    			.add(Restrictions.eq("buscarPorUsuario.nick", "nick1")).uniqueResult();
-        
-        System.out.println(socio.getNombre());
-        
-    }
-    
-    @SuppressWarnings({ "unused", "unchecked" })
-	@Test
-    @Transactional @Rollback(true)
-    public void listarSucursal(){
-    	Session sesion = getSession();
-        
-              
-        Sucursal sucursal1 = new Sucursal();
-        sucursal1.setId(1L);
-        sucursal1.setNombre("suc1");
-        
-        Sucursal sucursal2 = new Sucursal();
-        sucursal1.setId(2L);
-        sucursal2.setNombre("suc2");
-        
-        sesion.save(sucursal1);
-        sesion.save(sucursal2);
-        
-        List<Sucursal> lista = sesion.createCriteria(Sucursal.class)
-				.list();
-        for (Sucursal sucursal : lista) {
-			System.out.println(sucursal.getNombre());
-		}
-    }
-    */
 	
-	@Transactional @Rollback(true)
-    @Test
-    public void testQueMuestreQR() {
-    	CodigoQR codigo = new CodigoQR();
-    	String dato = "Esto es un Codigo QR";
-    	codigo.generarQR(dato);
-    	System.out.println(codigo);
-    }
+//	@Transactional @Rollback(true)
+//    @Test
+//    public void testQueMuestreQR() {
+//    	CodigoQR codigo = new CodigoQR();
+//    	String dato = "Esto es un Codigo QR";
+//    	codigo.generarQR(dato);
+//    	System.out.println(codigo);
+//    }
     
     
 }
--- gimnasio/src/test/java/ar/edu/unlam/tallerweb1/persistencia/TestMock.java
+++ gimnasio/src/test/java/ar/edu/unlam/tallerweb1/persistencia/TestMock.java
@@ -2,7 +2,9 @@ package ar.edu.unlam.tallerweb1.persistencia;
 
 import ar.edu.unlam.tallerweb1.SpringTest;
 import ar.edu.unlam.tallerweb1.controladores.ControladorLogin;
+import ar.edu.unlam.tallerweb1.modelo.Pase;
 import ar.edu.unlam.tallerweb1.modelo.Socio;
+import ar.edu.unlam.tallerweb1.modelo.Sucursal;
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
 import ar.edu.unlam.tallerweb1.servicios.ServicioLogin;
 import ar.edu.unlam.tallerweb1.servicios.ServicioSocio;
@@ -28,31 +30,112 @@ import static org.mockito.Mockito.*;
 
 public class TestMock extends SpringTest{
   
+//    @Test
+//    @Transactional @Rollback(true)
+//    public void pruebaControladorLogin(){
+//    	ControladorLogin controladorLogin = new ControladorLogin(); //Como es el que testeo no es mock
+//    	Usuario usuarioMock = mock (Usuario.class);
+//    	HttpServletRequest requestMock = mock(HttpServletRequest.class);
+//    	HttpSession sessionMock = mock(HttpSession.class);
+//    	ServicioLogin servicioLoginMock = mock (ServicioLogin.class);
+//    	controladorLogin.setServicioLogin(servicioLoginMock);
+//    	ServicioSocio servicioSocioMock = mock(ServicioSocio.class);
+//    	controladorLogin.setServicioSocio(servicioSocioMock);
+//    	Socio socioMock = mock (Socio.class);
+//    	
+//    	when(servicioLoginMock.consultarUsuario(usuarioMock)).thenReturn(usuarioMock);
+//    	when(requestMock.getSession()).thenReturn(sessionMock);
+//    	when(usuarioMock.getRol()).thenReturn(null);
+//    	
+//    	when(servicioSocioMock.buscarSocio(usuarioMock)).thenReturn(socioMock);
+//    	when(socioMock.getNombre()).thenReturn("test");
+//    	    	
+//    	doNothing().when(sessionMock).setAttribute("test", "test");
+//    	
+//    	ModelAndView modelAndView = controladorLogin.validarLogin(usuarioMock, requestMock);
+//    	assertThat(modelAndView.getViewName()).isEqualTo("redirect:/home");
+//    	
+//    }
+    
     @Test
     @Transactional @Rollback(true)
-    public void pruebaControladorLogin(){
+    public void pruebaControladorAlLogin(){
     	ControladorLogin controladorLogin = new ControladorLogin(); //Como es el que testeo no es mock
     	Usuario usuarioMock = mock (Usuario.class);
     	HttpServletRequest requestMock = mock(HttpServletRequest.class);
-    	HttpSession sessionMock = mock(HttpSession.class);
+    	
     	ServicioLogin servicioLoginMock = mock (ServicioLogin.class);
     	controladorLogin.setServicioLogin(servicioLoginMock);
     	ServicioSocio servicioSocioMock = mock(ServicioSocio.class);
     	controladorLogin.setServicioSocio(servicioSocioMock);
-    	Socio socioMock = mock (Socio.class);
+    	
+    	when(servicioLoginMock.consultarUsuario(usuarioMock)).thenReturn(null);
+    	ModelAndView modelAndView = controladorLogin.validarLogin(usuarioMock, requestMock);
+    	assertThat(modelAndView.getViewName()).isEqualTo("login");
+    }
+    
+    @Test
+    @Transactional @Rollback(true)
+    public void pruebaControladorAlHomeAdmin(){
+    	ControladorLogin controladorLogin = new ControladorLogin();
+    	HttpServletRequest requestMock = mock(HttpServletRequest.class);
+    	Usuario usuarioMock = mock(Usuario.class);
+    	ServicioLogin servicioLoginMock = mock(ServicioLogin.class);
+    	controladorLogin.setServicioLogin(servicioLoginMock);
     	
     	when(servicioLoginMock.consultarUsuario(usuarioMock)).thenReturn(usuarioMock);
-    	when(requestMock.getSession()).thenReturn(sessionMock);
-    	when(usuarioMock.getRol()).thenReturn(null);
+    	when(usuarioMock.getRol()).thenReturn("test");
+
+    	HttpSession sesionMock = mock(HttpSession.class);
+    	when(requestMock.getSession()).thenReturn(sesionMock);
+    	doNothing().when(sesionMock).setAttribute("test","test"); //va una sola vez aunque se llame varias veces
+    	    	    	
+    	ModelAndView vista = controladorLogin.validarLogin(usuarioMock, requestMock);
+    	assertThat(vista.getViewName()).isEqualTo("redirect:/homeAdmin");
     	
-    	when(servicioSocioMock.buscarSocio(usuarioMock)).thenReturn(socioMock);
-    	when(socioMock.getNombre()).thenReturn("test");
-    	    	
-    	doNothing().when(sessionMock).setAttribute("test", "test");
-    	
-    	ModelAndView modelAndView = controladorLogin.validarLogin(usuarioMock, requestMock);
-    	assertThat(modelAndView.getViewName()).isEqualTo("redirect:/home");
-    	
+		
     }
+    
+    @Test
+    @Transactional @Rollback(true)
+    public void pruebaControladorAlHome(){
+    	ControladorLogin controladorLogin = new ControladorLogin();
+    	HttpServletRequest requestMock = mock(HttpServletRequest.class);
+    	Usuario usuarioMock = mock(Usuario.class);
+    	ServicioLogin servicioLoginMock = mock(ServicioLogin.class);
+    	ServicioSocio servicioSocioMock = mock(ServicioSocio.class);
+    	controladorLogin.setServicioLogin(servicioLoginMock);
+    	controladorLogin.setServicioSocio(servicioSocioMock);
+    	
+    	when(servicioLoginMock.consultarUsuario(usuarioMock)).thenReturn(usuarioMock);
+    	when(usuarioMock.getRol()).thenReturn(null);
+
+    	
+    	
+    	Socio socioMock = mock(Socio.class);
+    	when(servicioSocioMock.buscarSocio(usuarioMock)).thenReturn(socioMock);
+    	Sucursal sucursalMock = mock(Sucursal.class);
+    	when(socioMock.getSucursal()).thenReturn(sucursalMock);
+    	when(sucursalMock.getId()).thenReturn(1L);
+    	
+    	when(servicioSocioMock.buscarSocio(usuarioMock).getNombre()).thenReturn("test");
+    	when(servicioSocioMock.buscarSocio(usuarioMock).getIdSocio()).thenReturn(1L);
+    	
+    	Pase paseMock = mock(Pase.class);
+    	when(socioMock.getPase()).thenReturn(paseMock);
+    	when(paseMock.getId()).thenReturn(1L);
+    	
+    	HttpSession sesionMock = mock(HttpSession.class);
+    	when(requestMock.getSession()).thenReturn(sesionMock);
+    	
+    	doNothing().when(sesionMock).setAttribute("test","test");
+    	    	
+    	ModelAndView vista = controladorLogin.validarLogin(usuarioMock, requestMock);
+    	assertThat(vista.getViewName()).isEqualTo("redirect:/home");
+    	
+		
+    }
+    
+    
    
 }
