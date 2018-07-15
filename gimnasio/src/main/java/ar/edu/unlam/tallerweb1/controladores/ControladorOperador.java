42563bd3a3d992a5b34d47d87f73fbc22cc7f087
florm <florenciamartin05@gmail.com>
2018-07-15 12:37:26 -0300

    home operador

--- gimnasio/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorOperador.java
+++ gimnasio/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorOperador.java
@@ -8,6 +8,7 @@ import org.junit.runner.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 import ar.edu.unlam.tallerweb1.modelo.Operador;
 import ar.edu.unlam.tallerweb1.modelo.Sucursal;
 import ar.edu.unlam.tallerweb1.servicios.ServicioOperador;
+import ar.edu.unlam.tallerweb1.servicios.ServicioSocio;
 import ar.edu.unlam.tallerweb1.servicios.ServicioSucursal;
 
 @Controller
@@ -25,6 +27,16 @@ public class ControladorOperador {
 	private ServicioOperador servicioOperador;
 	@Inject
 	private ServicioSucursal servicioSucursal;
+	@Inject
+	private ServicioSocio servicioSocio;
+	
+	
+	@RequestMapping(path = "/homeOperador/{idSucursal}")
+	public ModelAndView irAHomeOperador(@PathVariable (value="idSucursal") Long idSucursal) {
+		ModelMap modelo = new ModelMap();
+		modelo.put("listaSocios", servicioSocio.buscarSocios(idSucursal));
+		return new ModelAndView("homeOperador", modelo);
+	}
 	
 	@RequestMapping(path="operadores")
 	public ModelAndView listaOperadores(){
@@ -37,7 +49,7 @@ public class ControladorOperador {
 	public ModelAndView modificar(@RequestParam(value="operador") Long idOperador){
 		ModelMap modelo = new ModelMap();
 		Operador operador = servicioOperador.getById(idOperador);
-		Operador operadorUpdate = new Operador();
+		//Operador operadorUpdate = new Operador();
 		modelo.put("operador", operador);
 		List<Sucursal> listaSucursales = servicioSucursal.listarSucursales();
 		modelo.put("listaSucursales", listaSucursales);
