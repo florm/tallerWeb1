a26a8f5a55bb3c33ea6161dacd33a7329d9bbac5
AlejandraCasuscelli <alejandracasuscelli@gmail.com>
2018-07-15 22:27:16 -0300

    modifico vista de agregar sucursal y servicio, obtener latitud y longitud en proceso

--- gimnasio/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioSucursalImp.java
+++ gimnasio/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioSucursalImp.java
@@ -7,7 +7,9 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ar.edu.unlam.tallerweb1.dao.LocalizacionDao;
 import ar.edu.unlam.tallerweb1.dao.SucursalDao;
+import ar.edu.unlam.tallerweb1.modelo.Ciudad;
 import ar.edu.unlam.tallerweb1.modelo.Sucursal;
 
 
@@ -17,6 +19,9 @@ public class ServicioSucursalImp implements ServicioSucursal {
 	
 	@Inject
 	private SucursalDao sucursalDao;
+	
+	@Inject
+	private LocalizacionDao localizacionDao;
 
 	@Override
 	public List<Sucursal> listarSucursales() {
@@ -40,6 +45,10 @@ public class ServicioSucursalImp implements ServicioSucursal {
 	
 	@Override
 	public void agregarSucursal(Sucursal sucursalNueva) {
+		Ciudad ciudad = localizacionDao.traerCiudad(sucursalNueva.getCiudad().getId());
+		sucursalNueva.setCiudad(ciudad);
+		sucursalNueva.setPais("Argentina");
+		sucursalDao.agregarSucursal(sucursalNueva);
 		sucursalDao.agregarSucursal(sucursalNueva);
 	}
 
